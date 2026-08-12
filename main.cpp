821a55745ed4af57e2259196e6207331e7bf1243
KAAZamapiloT <dev352418@gmail.com>
2026-08-12 20:01:26 +0530

    Commit new

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 
-
+/// <summary>
+///  NO NEED FOR MAIN FUNCTION , THIS IS JUST A TEST FILE FOR THE LIBRARY
+/// </summary>
+/// <returns></returns>
 
 int main(){
     std::cout<<"Welcome to volt"<<std::endl;
