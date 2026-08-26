acc5165b946fb69f4a7e59c6d593b29726a168a8
KAAZamapiloT <dev352418@gmail.com>
2026-08-26 22:08:49 +0530

    update

--- include/volt/Containers/map.hpp
+++ include/volt/Containers/map.hpp
@@ -9,7 +9,9 @@ namespace volt {
 
 	template<typename K,typename V>
 	class map {
-	
+		static constexpr bool RED = false;
+		static constexpr bool BLACK = true;
+
 		struct DataNode {
 			K KEY;
 			V DATA;
@@ -24,13 +26,16 @@ namespace volt {
 	
 	public:
 		map() {
-
+			root_ = nullptr;
 		}
 
-		void insert(K key,V value) {
+		bool insert(K key,V value) {
 			
 			DataNode* node = bst_insert(key, value);
 
+			if (!node) {
+				return false;
+			}
 	
 			node->color = RED;
 
@@ -39,25 +44,66 @@ namespace volt {
 
 			root_->color = BLACK;
 
-
+			return true;
 		}
 		void remove(K key) {
 
 		}
+		
 		V get_key(K key) {
+			DataNode* trav = root_;
+			while (trav) {
+				parent = trav;
+				if (trav->KEY < key) {
+					trav = trav->right;
+				}
+				else if (trav->KEY > key) {
+					trav = trav->left;
+				}
+				else {
+					return trav->DATA;
+				}
 
+			}
+
+			// returningn a dummy object
+			V obj;
+			return obj;
 		}
 
 
+		operator [](K key) {
+			DataNode* trav = root_;
+			while (trav) {
+				parent = trav;
+				if (trav->KEY < key) {
+					trav = trav->right;
+				}
+				else if (trav->KEY > key) {
+					trav = trav->left;
+				}
+				else {
+					return trav->DATA;
+				}
 
+			}
+			// not present insert it key
+			V val;
+			insert(key, val);
+			return val;
+		}
 	private:
 		DataNode* root_;
 
 		DataNode* bst_insert( K key, V value) {
-			DataNode* val = new DataNode();
-			val->KEY = key;
-			val->DATA = value;
-			DataNode* trav = root_{nullptr};
+			DataNode* node = new DataNode();
+			node->KEY = key;
+			node->DATA = value;
+			node->left = nullptr;
+			node->right = nullptr;
+			node->color = RED;
+
+			DataNode* trav = root_;
 			DataNode* parent = nullptr;
 			while (trav) {
 				parent = trav;
@@ -68,29 +114,11 @@ namespace volt {
 					trav = trav->left;
 				}
 				else {
-					// replace the current node in map
-					node->parent = trav->parent;
-					if (trav->parent->left == trav) {
-						trav->parent->left = node;
-					}
-					else if (trav->parent->right == trav) {
-						trav->parent->right = node;
-					}
-
-					if (trav->left) {
-						trav->left->parent = node;
-					}
-					if (trav->right) {
-						trav->right->parent = node;
-					}
-					node->left = trav->left;
-					node->right = trav->right;
-
-					return node;
+					return nullptr;
 				}
 				
 			}
-			val->parent = parent;
+			node->parent = parent;
 	 
 			if (!parent) {
 				root_ = node;
@@ -101,19 +129,56 @@ namespace volt {
 			else {
 				parent->left = node;
 			}
-			return val;
+			return node;
 		}
 		
 
 		void insert_fixup(DataNode* node) {
-			if (node->parent->color != node->color) {
-				return;
-			}
-			if (node->parent == nullptr) {
+			if ((node->parent == nullptr)||(node->parent->color != node->color)) {
 				return;
 			}
 			
-			
+			// if parent is red 
+			while (node != root_ && node->parent->color == RED) {
+				if (node->parent == node->parent->parent->left) {
+					DataNode* y = node->parent->parent->right;
+					if (y&&!y->color) {
+						node->parent->color = BLACK;
+						y->color = BLACK;
+						node->parent->parent->color = false;
+						node = node->parent->parent;
+
+					}
+					else if (node == node->parent->right) {
+						node = node->parent;
+						rotate_left(node);
+						node->parent->color = BLACK;
+
+						node->parent->parent->color = RED;
+
+						rotate_right(node->parent->parent);
+
+					}
+				}
+				else {
+					DataNode* y = node->parent->parent->left;
+					if (y&&!y->color) {
+						node->parent->color = BLACK;
+						y->color = BLACK;
+						node->parent->parent->color = RED;
+						node = node->parent->parent;
+
+					}
+					else if (node == node->parent->left) {
+						node = node->parent;
+						rotate_left(node);
+						node->parent->color = BLACK;
+						node->parent->parent->color = RED;
+						rotate_right(node->parent->parent);
+					}
+				}
+			}
+			root_->color = BLACK;
 		}
 
 		void rotate_left(DataNode* x) {
@@ -146,7 +211,7 @@ namespace volt {
 			DataNode* y = x->left;
 			x->left = y->right;
 
-			if (x->right) {
+			if (y->right) {
 				y->right->parent = x;
 			}
 	
